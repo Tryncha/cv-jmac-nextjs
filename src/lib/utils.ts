ba7e114ac625f8a6117d1b840d8a181dc5dfbfcc
Tryncha <juanes.aguirre26@gmail.com>
2026-03-26 15:34:05 -0500

    publications page with searchParams created

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,4 @@
+import { useLocale, useTranslations } from 'next-intl';
 import { getLocale, getTranslations } from 'next-intl/server';
 
 export function capitalizeWord(word: string) {
@@ -5,9 +6,18 @@ export function capitalizeWord(word: string) {
   return normalizedWord[0].toUpperCase + normalizedWord.slice(1, normalizedWord.length);
 }
 
+// Server components
 export async function getIntlConfig() {
   const t = await getTranslations();
   const locale = await getLocale();
+
+  return { t, locale };
+}
+
+// Client components
+export function useIntlConfig() {
+  const t = useTranslations();
+  const locale = useLocale();
 
   return { t, locale };
 }
